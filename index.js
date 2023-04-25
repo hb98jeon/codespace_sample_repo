c1cca19ec9c5d51549360271547f8d7e7c330ad2
hb98jeon <130533538+hb98jeon@users.noreply.github.com>
2023-04-25 07:15:45 +0000

    Modify console logs

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('./app')
 const {PORT} = process.env
 app.listen(PORT, () => {
-    console.log('SERVER is running at port: ${PORT');
+    console.log(`SERVER is running at port: ${PORT}`);
 })
